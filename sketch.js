d27c061826b779337968741c48eb30a22632dda0
raul4247 <raulfmansur@hotmail.com>
2019-03-27 17:18:16 -0300

    change of color on dvd logo

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ let canvasWidth, canvasHeight
 let x, y
 let xspeed, yspeed
 let dvd
+let r, g, b
 
 function preload() {
     dvd = loadImage('imgs/dvd.png')
@@ -15,14 +16,18 @@ function setup() {
     xspeed = 5
     yspeed = 5
 
+    r = random(100, 256)
+    g = random(100, 256)
+    b = random(100, 256)
+    
     createCanvas(canvasWidth, canvasHeight)    
 }
 
 function draw(){
     background('black')
 
+    tint(r, g, b)
     image(dvd, x, y)
-
     moveLogo()
 }
 
@@ -33,17 +38,27 @@ function moveLogo(){
     if(x >= canvasWidth-dvd.width){
         xspeed = -xspeed
         x = canvasWidth-dvd.width
+        changeColor()
     }
     else if(x <= 0){
         xspeed = -xspeed
         x = 0
+        changeColor()
     }
     if(y >= canvasHeight-dvd.height){
         yspeed = -yspeed
         y = canvasHeight-dvd.height
+        changeColor()
     }
     else if(y <= 0){
         yspeed = -yspeed
         y = 0
+        changeColor()
     }
+}
+
+function changeColor() {
+    r = random(100, 256)
+    g = random(100, 256)
+    b = random(100, 256)
 }
